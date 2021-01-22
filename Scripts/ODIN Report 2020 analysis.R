3fc09ff38e5f0cea7b2bdb6a3c391d32033ae808
Lorenz <lorenznoe@gmail.com>
2021-01-22 13:41:48 -0500

    Annotate code, transform to long, and clean elements variable

--- Scripts/ODIN Report 2020 analysis.R
+++ Scripts/ODIN Report 2020 analysis.R
@@ -11,10 +11,15 @@ odin_scores <- read_csv("Input/ODIN_scores_2020.csv") %>%
               janitor::clean_names() %>%
               # Take out missing observations at bottom of the table, it's just metadata on copyright
               filter(!is.na(year))) %>%
+  # Clean variables and data categories to be able to compare 2020 to 2018
   mutate(second_administrative_level = as.numeric(str_remove(second_administrative_level, "-")),
          data_categories = str_remove_all(data_categories, "\\n$|\\s$"),
          data_categories = case_when(
            data_categories == "Energy use" ~ "Energy",
            data_categories == "Land use" ~ "Agriculture & Land Use",
            TRUE ~ data_categories
-         ))
+         )) %>%
+  # Convert to long
+  pivot_longer(indicator_coverage_and_disaggregation:overall_score, names_to = "elements", values_to = "score") %>%
+  # Convert elements back to sentence case for easier reading
+  mutate(elements = str_to_sentence(str_replace_all(elements, "_", " ")))

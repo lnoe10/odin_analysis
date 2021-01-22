ccf24125e3ed75ababb40cc89d2eadeab03250cc
Lorenz <lorenznoe@gmail.com>
2021-01-22 11:04:15 -0500

    Load scores and clean variables

--- Scripts/ODIN Report 2020 analysis.R
+++ Scripts/ODIN Report 2020 analysis.R
@@ -1,1 +1,14 @@
 library(tidyverse)
+
+odin_scores <- read_csv("Input/ODIN_scores_2020.csv") %>%
+  # convert all variable names to snakecase
+  janitor::clean_names() %>%
+  # Take out missing observations at bottom of the table, it's just metadata on copyright
+  filter(!is.na(year)) %>%
+  # Add 2018 scores
+  bind_rows(read_csv("Input/ODIN_scores_2018.csv") %>%
+              # convert all variable names to snakecase
+              janitor::clean_names() %>%
+              # Take out missing observations at bottom of the table, it's just metadata on copyright
+              filter(!is.na(year))) %>%
+  mutate(second_administrative_level = as.numeric(str_remove(second_administrative_level, "-")))

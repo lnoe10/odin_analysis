8b92614bce32d9008258acb1be6c422312e1038f
Lorenz <lorenznoe@gmail.com>
2021-01-25 16:46:04 -0500

    Add 2016 and 2017 ODIN data

--- Scripts/ODIN Report 2020 analysis.R
+++ Scripts/ODIN Report 2020 analysis.R
@@ -14,9 +14,21 @@ odin_scores <- read_csv("Input/ODIN_scores_2020.csv") %>%
               janitor::clean_names() %>%
               # Take out missing observations at bottom of the table, it's just metadata on copyright
               filter(!is.na(year))) %>%
+  mutate(second_administrative_level = as.numeric(str_remove(second_administrative_level, "-"))) %>%
+  # Add 2017 scores
+  bind_rows(read_csv("Input/ODIN_scores_2017.csv") %>%
+              # convert all variable names to snakecase
+              janitor::clean_names() %>%
+              # Take out missing observations at bottom of the table, it's just metadata on copyright
+              filter(!is.na(year))) %>%
+  # Add 2016 scores
+  bind_rows(read_csv("Input/ODIN_scores_2016.csv") %>%
+              # convert all variable names to snakecase
+              janitor::clean_names() %>%
+              # Take out missing observations at bottom of the table, it's just metadata on copyright
+              filter(!is.na(year))) %>%
   # Clean variables and data categories to be able to compare 2020 to 2018
-  mutate(second_administrative_level = as.numeric(str_remove(second_administrative_level, "-")),
-         data_categories = str_remove_all(data_categories, "\\n$|\\s$"),
+  mutate(data_categories = str_remove_all(data_categories, "\\n$|\\s$"),
          data_categories = case_when(
            data_categories == "Energy use" ~ "Energy",
            data_categories == "Land use" ~ "Agriculture & Land Use",
@@ -26,6 +38,7 @@ odin_scores <- read_csv("Input/ODIN_scores_2020.csv") %>%
   pivot_longer(indicator_coverage_and_disaggregation:overall_score, names_to = "element", values_to = "score") %>%
   # Convert elements back to sentence case for easier reading
   mutate(element = str_to_sentence(str_replace_all(element, "_", " ")),
+         # Create macro sector group for use in graphing/analysis
          macro_sector = case_when(
            data_categories %in% c("Population & vital statistics", "Education facilities",
                                   "Education outcomes", "Health facilities", "Health outcomes",
